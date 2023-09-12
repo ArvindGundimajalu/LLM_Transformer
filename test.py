84218a921f98d8141d349f76877dde7e84d7f75d
ArvindGundimajalu <arvindg12@gmail.com>
2023-09-13 01:06:40 +0530

    modified:   test.py

--- test.py
+++ test.py
@@ -4,7 +4,10 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 from langchain import HuggingFaceHub
 from langchain import PromptTemplate, LLMChain
-question = "Write a select query to find top 10 entries in table Employee "
+from datetime import datetime
+
+print(datetime.now())
+question = "find the top 10 car brands"
 
 template = """Question: {question}
 
@@ -12,10 +15,11 @@ Answer: Let's think step by step."""
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
 
-repo_id = "google/flan-t5-xxl"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+repo_id = "Amirhparizi/Text_to_SQL_BART_SIMPLE"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
 llm = HuggingFaceHub(
     repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 64}
 )
 llm_chain = LLMChain(prompt=prompt, llm=llm)
-
+print(datetime.now())
 print(llm_chain.run(question))
+print(datetime.now())
